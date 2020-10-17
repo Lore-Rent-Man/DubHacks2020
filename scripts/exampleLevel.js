6569b37750d3775dccceed1d34dd89481b52ff28
hackerkake <laurent.tian.man@gmail.com>
2020-10-17 14:49:24 -0700

    Added sprite class

--- scripts/exampleLevel.js
+++ scripts/exampleLevel.js
@@ -1,6 +1,8 @@
+player = new SpritePNG(0.1, 'scripts/spriteness.png', 'scripts/spriteness.json');
 const l1 = (p)=>{
     p.preload = function()
     {
+        player.preload(p);
         //Function to load sprites, textures, etc
     }
 
@@ -8,19 +10,22 @@ const l1 = (p)=>{
     {
         //Initialization of canvas and other code that needs to be run once at the beginning of the level
         p.createCanvas(app.windowWidth, app.windowHeight);
+        player.loadAnimation(p);
     }
 
     p.draw = function()
     {
         //Function that draws each frame
         p.background(220);
+        player.show(250, 250, 23, 4, p);
+        player.animate();
     }
 
     p.mousePressed = function()
     {
         //ALWAYS CALL THIS PIECE OF CODE AFTER CREATING A NEW LEVEL
-        p.remove();
+        //p.remove();
         //Example trigger for next level
-        app.nextLevel();
+        //app.nextLevel();
     }
 }
--- scripts/sprite.js
+++ scripts/sprite.js
@@ -1,10 +1,46 @@
-class Sprite{
-    constructor(speed, spritesheet, spritedata){
+class SpritePNG{
+    constructor(speed, spritelink, spritedata){
+        this.x = 0;
+        this.y = 0;
+
         this.speed = speed;
-        this.spritesheet = spritesheet;
+        this.spritelink = spritelink;
         this.spritedata = spritedata;
         this.animation = [];
+        this.index = 0;
+        this.spritesheet;
     }
 
-    
+    preload(p){
+        this.spritedata = p.loadJSON(this.spritedata);
+        this.spritesheet = p.loadImage(this.spritelink);
+    }
+
+    loadAnimation(){
+        if(this.spritedata)
+        {
+            let frames = this.spritedata.data;
+            for(let i=0;i<frames.length;i++)
+            {
+                let pos = frames[i];
+                let img = this.spritesheet.get(pos.x, pos.y, pos.w, pos.h);
+                this.animation.push(img);
+            }
+        }
+        else
+        {
+            this.animation.push(this.spritesheet);
+        }
+    }
+
+    show(posX, posY, offset, frameCount, p)
+    {
+        let index = offset + (p.floor(this.index) % frameCount);
+        p.image(this.animation[index], posX, posY);
+    }
+
+    animate()
+    {
+        this.index += this.speed;
+    }
 }
