9e7ae1d43d159539b80d02efcb6d8ca412968d3d
tianys5 <tianys5@cs.washington.edu>
2020-10-17 22:46:34 -0700

    indent

--- scripts/exampleLevel.js
+++ scripts/exampleLevel.js
@@ -19,6 +19,7 @@ const l1 = (p)=>{
         //Initialization of canvas and other code that needs to be run once at the beginning of the level
         p.createCanvas(app.windowWidth, app.windowHeight);
         player1.loadAnimations(p);
+        player1.setRespawnPoint(0, app.windowHeight - 32);
     }
 
     p.draw = function()
@@ -26,8 +27,8 @@ const l1 = (p)=>{
         //Function that draws each frame
         p.image(backgroundImg, -300, -300);
         p.fill(255, 204, 0);
-        p.rect(100, 430, 60, 10);
-        p.rect(200, 350, 60, 10);
+        p.rect(100, 430, 60, 10, 5);
+        p.rect(200, 350, 60, 10, 5);
         if(player1.posY > app.windowHeight - 32)
         {
             player1.posY = app.windowHeight - 32;
