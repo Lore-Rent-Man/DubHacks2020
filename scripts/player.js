6c2c3ed49cf242621e58128ba9135974f5eaf6dd
hackerkake <laurent.tian.man@gmail.com>
2020-10-17 19:52:46 -0700

    Added player death

--- scripts/player.js
+++ scripts/player.js
@@ -1,14 +1,15 @@
 class player{
     constructor(){
 
-        const idle = new BunnySprite(0.2, '../sprite_folders/pink_monster/Pink_Monster_Idle_4.png', 4);
-        const walk = new BunnySprite(0.2, '../sprite_folders/pink_monster/Pink_Monster_Walk_6.png', 6);
-        const jump = new BunnySprite(0.001, '../sprite_folders/pink_monster/Pink_Monster_Jump_8.png', 8);
-        const run  = new BunnySprite(0.2, '../sprite_folders/pink_monster/Pink_Monster_Run_6.png', 6);
+        const idle  = new BunnySprite(0.2, '../sprite_folders/pink_monster/Pink_Monster_Idle_4.png', 4);
+        const walk  = new BunnySprite(0.2, '../sprite_folders/pink_monster/Pink_Monster_Walk_6.png', 6);
+        const jump  = new BunnySprite(0.001, '../sprite_folders/pink_monster/Pink_Monster_Jump_8.png', 8);
+        const run   = new BunnySprite(0.2, '../sprite_folders/pink_monster/Pink_Monster_Run_6.png', 6);
+        const death = new BunnySprite(0.2, '../sprite_folders/pink_monster/Pink_Monster_Death_8.png', 8);
 
         this.speed = 3;
         this.gravity = 0.5;
-        this.numJumps = 2;
+        this.numJumps = 5;
 
         this.posX = 0;
         this.posY = 0;
@@ -17,15 +18,17 @@ class player{
         this.size = 0;
 
         this.animations = [];
-        this.animations.push(idle); //0 --- Idle
-        this.animations.push(walk); //1 --- Walk
-        this.animations.push(jump); //2 --- Jump
-        this.animations.push(run);  //3 --- Run
+        this.animations.push(idle);  //0 --- Idle
+        this.animations.push(walk);  //1 --- Walk
+        this.animations.push(jump);  //2 --- Jump
+        this.animations.push(run);   //3 --- Run
+        this.animations.push(death); //4 --- Death
 
         this.moveLeft = false;
         this.isJumping = false;
 
         this.isDead = false;
+        this.deadFrameCount = 0;
     }
 
     preloadSprites(p)
@@ -45,56 +48,74 @@ class player{
 
         p.keyPressed = () =>
         {
+            console.log(p.keyCode);
             if(p.keyCode == p.UP_ARROW && ((!this.isJumping) || this.numJumps != 0))
             {
                 this.velocityY = -8;
                 this.isJumping = true;
                 this.numJumps--;
             }
+            if(p.keyCode == 82 && this.deadFrameCount >= 8/0.2)
+            {
+                this.isDead = false;
+                this.deadFrameCount = 0;
+            }
+            if(p.keyCode == 65)
+            {
+                this.isDead = true;
+            }
         }
     }
 
     draw(p)
     {
-        
-        if (p.keyIsDown(p.LEFT_ARROW)) {
-            this.velocityX = -this.speed;
-            this.moveLeft = true;
-        }
-        else if (p.keyIsDown(p.RIGHT_ARROW)) {
-            this.velocityX = this.speed;
-            this.moveLeft = false;
-        }
-        else if (p.keyIsDown(p.DOWN_ARROW)){
-            this.velocityY = 
+        if(!this.isDead)
+        {
+            if (p.keyIsDown(p.LEFT_ARROW)) {
+                this.velocityX = -this.speed;
+                this.moveLeft = true;
+            }
+            else if (p.keyIsDown(p.RIGHT_ARROW)) {
+                this.velocityX = this.speed;
+                this.moveLeft = false;
+            }else
+            {
+                this.velocityX = 0;
+            }
+            if (p.keyIsDown(p.DOWN_ARROW)){
+                this.velocityY = 8;
+            }
+            this.velocityY += this.gravity;
+    
+            this.posX += this.velocityX;
+            this.posY += this.velocityY;
+    
+            if(player1.posY > app.windowHeight - 32)
+            {
+                player1.posY = app.windowHeight - 32;
+                this.isJumping = false;
+                this.numJumps = 5;
+            }
+            if(p.isJumping)
+            {
+                this.drawAction(p, 2);
+            }
+            else if((p.keyIsDown(p.LEFT_ARROW) || p.keyIsDown(p.RIGHT_ARROW) || p.keyIsDown(p.UP_ARROW) || p.keyIsDown(p.DOWN_ARROW)))
+            {
+                this.drawAction(p, 3);
+            }
+            else
+            {
+                this.drawAction(p, 0);
+            }
         }
         else
         {
-            this.velocityX = 0;
-        }
-
-        this.velocityY += this.gravity;
-
-        this.posX += this.velocityX;
-        this.posY += this.velocityY;
-
-        if(player1.posY > app.windowHeight - 32)
-        {
-            player1.posY = app.windowHeight - 32;
-            this.isJumping = false;
-            this.numJumps = 2;
-        }
-        if(p.isJumping)
-        {
-            this.drawAction(p, 2);
-        }
-        else if((p.keyIsDown(p.LEFT_ARROW) || p.keyIsDown(p.RIGHT_ARROW) || p.keyIsDown(p.UP_ARROW) || p.keyIsDown(p.DOWN_ARROW)))
-        {
-            this.drawAction(p, 3);
-        }
-        else
-        {
-            this.drawAction(p, 0);
+            if(this.deadFrameCount < 8/0.2)
+            {
+                this.drawAction(p, 4);   
+                this.deadFrameCount++;
+            }
         }
     }
 
